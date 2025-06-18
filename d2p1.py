90d7d613c703d64382ecc1a29011b20aa689a37b
Edward Martinez <emartinezj1802@gmail.com>
2025-06-18 15:32:08 -0500

    tipos de datos dia 2

---
+++ d2p1.py
@@ -0,0 +1,12 @@
+# TIPOS DE DATOS #
+
+"""
+string (str)        "hola","%$&"," ","123"  --- cadenas de texto siempre van entre comillas
+Integer (int)       150,1,555,-15,0 --- numeros enteros positivos o negativos NO deben ir entre comillas
+floating (float)    1.25,25.1,500.50,-95.3 --- numeros con decimales
+listas (lst)        ["sal,1,-3,4.5,"marte"] --- van entre corchetes y puede contener cualquier tipo de dato
+diccionarios (dic)  {'color':'rojo','arte':'cine'} --- van entre llaves tienen como un valor y su significado
+tuples (tup)        ('lun','mar','mie','jue','vie') --- entre parentesis parecido a las listas de cualquier tipo de datos pero su orden es inmutable no puede odificarse
+set (set)           {'a','b','c','d','e'} --- conjuto ordenadode elementos unicose irrepetible
+booleanos (bool)    True,False --- valores verdaderos o falsos
+"""

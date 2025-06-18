483e941d73d6f9286cb5e460ea6a012d07724e8c
Edward Martinez <emartinezj1802@gmail.com>
2025-06-18 16:08:39 -0500

    ejercicios con variables

---
+++ d2p2.py
@@ -0,0 +1,37 @@
+# VARIABLES #
+
+"""
+Se puede hacer una similitud de una variable con una caja que contiene un elemento,
+ejemplo una caja que seria mi variable y dentro de la caja esta mi libro favorito,
+es decir, es un espacio dde memoria reservado para asignarle un contenido o valor que se puede cambiar asi como puede cabiar mi libro favorito
+"""
+
+# ejemplo #
+
+nombre =  "juan" #se asigna un nombre a una variable llamada "nombre" y se le da el valor juan#
+print(nombre)
+
+nombre = "Laura" #se cambia el nombre de la variable y se le asigna un nuevo valor#
+print(nombre)
+
+edad = 30 #se asigna una variable un tipo de dato tipo (int)#
+print(edad)
+
+edad2 = 15 # nueva variable tipo (int) #
+print(edad2)
+
+print(edad + edad2) # suma de dos variables #
+
+
+# ejemplo 2 #
+
+nombre = input("dime tu nombre ")
+apellido = input("dime tu apellido ")
+print ("tu nombre es " + nombre + " " + apellido)
+
+# ejemplo 3 #
+
+p1 = "hola "
+p2 = "python"
+frase = p1 + p2
+print(frase)
